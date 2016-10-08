e37b36eeaaf369e3f63181159c7d071050a83023
Ryan Samp <ryan.samp@c3iot.com>
2016-10-08 14:50:33 -0700

    Add/Edit Author page

---
+++ react_flux/src/components/authors/authorForm.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var React = require('react');
+var TextInput = require('../common/textInput');
+
+var AuthorForm = React.createClass({
+  propTypes: {
+    author: React.PropTypes.object.isRequired,
+    onSave: React.PropTypes.func.isRequired,
+    onChange: React.PropTypes.func.isRequired,
+    errors: React.PropTypes.object
+  },
+
+  render: function() {
+    return (
+      <form>
+        <TextInput
+          name="firstName"
+          label="First Name"
+          placeholder="First Name"
+          value={this.props.author.firstName}
+          onChange={this.props.onChange}
+          error={this.props.errors.firstName} />
+
+        <TextInput
+          name="lastName"
+          label="Lase Name"
+          placeholder="Lase Name"
+          value={this.props.author.lastName}
+          onChange={this.props.onChange}
+          error={this.props.errors.lastName} />
+
+        <input type="submit" value="Save" className="btn btn-default" onClick={this.props.onSave} />
+      </form>
+    );
+  }
+});
+
+module.exports = AuthorForm;
---
+++ react_flux/src/components/authors/manageAuthorPage.js
@@ -0,0 +1,89 @@
+"use strict";
+
+var React = require('react');
+var Router = require('react-router');
+var AuthorForm = require('./authorForm');
+var AuthorApi = require('../../api/authorApi');
+var toastr = require('toastr');
+
+var ManageAuthorPage = React.createClass({
+  mixins: [
+    Router.Navigation
+  ],
+
+  statics: {
+    willTransitionFrom: function(transition, component) {
+      if (component.state.dirty && !confirm('Leave without saving?')) {
+        transition.abort();
+      }
+    }
+  },
+
+  getInitialState: function() {
+    return {
+      author: { id: '', firstName: '', lastName: '' },
+      errors: {},
+      dirty: false
+    };
+  },
+
+  componentWillMount: function() {
+    var authorId = this.props.params.id; //from the path '/author:id'
+
+    if (authorId) {
+      this.setState({author: AuthorApi.getAuthorById(authorId)});
+    }
+  },
+
+  setAuthorState: function(e) {
+    this.setState({dirty: true});
+    var field = e.target.name;
+    var value = e.target.value;
+    this.state.author[field] = value;
+    return this.setState({author: this.state.author});
+  },
+
+  authorFormIsValid: function() {
+    var formIsValid = true;
+    this.state.errors = {}; //clear any previous errors
+    if (this.state.author.firstName.length < 3) {
+      this.state.errors.firstName = 'First name must be at least 3 characters';
+      formIsValid = false;
+    }
+
+    if (this.state.author.lastName.length < 3) {
+      this.state.errors.lastName = 'Last name must be at least 3 characters';
+      formIsValid = false;
+    }
+
+    this.setState({errors: this.state.errors});
+    return formIsValid;
+  },
+
+  saveAuthor: function(e) {
+    e.preventDefault();
+
+    if (!this.authorFormIsValid()) {
+      return;
+    }
+    AuthorApi.saveAuthor(this.state.author);
+    this.setState({dirty: false});
+    toastr.success('Author saved');
+    this.transitionTo('authors');
+  },
+
+  render: function() {
+    return (
+      <div>
+        <h1>Manage Author</h1>
+        <AuthorForm
+          author={this.state.author}
+          onChange={this.setAuthorState}
+          onSave={this.saveAuthor}
+          errors={this.state.errors} />
+      </div>
+    );
+  }
+});
+
+module.exports = ManageAuthorPage;
